af521ae0ef201fdbddaa0204c7d252b4265bdb96
mhff <frank0319@gmail.com>
2017-10-04 21:13:15 +0800

    flexible feature numbers

--- hw1/linear_train.py
+++ hw1/linear_train.py
@@ -5,7 +5,7 @@
 import sys
 import numpy as np
 
-num_w = 162
+num_w = 162 # default is to use all features
 
 def main(argv):
 
@@ -23,8 +23,11 @@ def main(argv):
                 x_data.append(tmp[1:])
                 y_data.append(tmp[0])
     
+    # x_data: N by I matrix (N=#entries,I=#features)
+    # y_data: N-d vector
     x_data = np.array(x_data).astype(float)
     y_data = np.array(y_data).astype(float)
+    num_w = x_data.shape[1]
 
     # randomly determine model parameters
     # L2_regularization:
@@ -34,10 +37,12 @@ def main(argv):
     w = np.random.randn(num_w)/1000
     ld = 0.001  # lambda for regularization
     
-    # Apply AdaGrad (fixed rate lr = 0.00000000035 works well)
+    # Apply AdaGrad
+    # if fixed rate, lr = 0.00000000035 works well
     lr = 1  # learning rate
     iteration = 10000
     
+    # lr_b, lr_w are sigmas for AdaGrad
     lr_b = 0.0
     lr_w = np.zeros(num_w)
     
@@ -49,17 +54,19 @@ def main(argv):
         
         # define loss function as square error
         # predicted value y from nth data = nth entry of X*w+b
-        y_predict = np.dot(x_data,w) + b + ld*np.sum(np.power(w,2)) # latter terms are scalors
+        y_predict = np.dot(x_data,w) + b + ld*np.sum(np.power(w,2)) # latter two terms are scalors
         dy = y_data - y_predict
         
         # check current loss
         loss = sum(np.power(dy,2))
         print('current loss:',loss)
         
+        # Compute gradients
         b_grad -= np.sum(2*dy)
         for i in range(num_w):
             w_grad[i] -= np.dot( (2*dy) , (x_data.T[i]+2*ld*w[i]) )
-            
+        
+        # Compute sigmas for AdaGrad
         lr_b += b_grad ** 2
         lr_w += np.power(np.array(w_grad),2)
         
@@ -68,7 +75,7 @@ def main(argv):
         for i in range(num_w):
             w[i] = w[i]-lr/np.sqrt(lr_w[i])*w_grad[i]
     
-    print('end with total loss:',loss)
+    print('Training ended with total loss:',loss)
     
     # test with validation set
     print('=====validation=====')
@@ -90,8 +97,8 @@ def main(argv):
     dy = y_data - y_predict
     loss = sum(np.power(dy,2))
     rmse = np.sqrt(sum(np.power(dy,2))/len(x_data))
-    print('Total loss on validation set: ',loss)
-    print('RMSE on validation set: ',rmse)
+    print('Total loss on validation set:',loss)
+    print('RMSE on validation set:',rmse)
     
     with open(argv[3], 'w') as output:
         line = str(b)+','+','.join(list(w.astype(str)))
