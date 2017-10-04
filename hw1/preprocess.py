efcd5d7ce88f3ba9365af93f8aa77fff39e91c4a
mhff <frank0319@gmail.com>
2017-10-04 22:42:22 +0800

    customizable preprocessor

--- hw1/preprocess.py
+++ hw1/preprocess.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 # Data Pre-procession
-# usage ./preprocess.py [input] [training_set] [validation_set]
+# usage: ./preprocess.py [input] [training] [validation]
 
+### File Format ### 
+#
+# [input]:       train.csv
+# [training]:    1st line reads used feature-row indices,
+#                followed by lines starts with ground-true
+#                y_hat, proceed with feature x's
+# [validation]:  lines starts with ground-true y proceed with
+#                feature x's
+#
+###################
+          
 import sys
 import numpy as np
 import pandas as pd
 
-num_feature = 18
-num_prev_hour = 5
+feature_idx = set([9,10])   # Set which features to use
+num_feature = len(feature_idx)
+num_feature_t = 18
+
+feature_dropped_idx = set(range(18))-feature_idx  # type:set
+
+num_prev_hour = 9
 day_per_month = 20
 
 # PM2.5 values are on the 9th row of each day
@@ -18,24 +34,32 @@ def main(argv):
     train_out = open(argv[2],'w')
     val_out = open(argv[3],'w')
     
-    # replace 'NR' as 0
+    # Write used feature indices on first line
+    train_out.write(','.join([str(i) for i in feature_idx])+'\n')
+    
+    # Replace 'NR' as 0.0
     train_df = train_df.replace({'NR':'0.0'})
     
-    # consider no cross-date relation
-    num_day = len(train_df) // num_feature
+    # Consider no cross-date relation
+    num_day = len(train_df) // num_feature_t
     for d in range(num_day):
 
-        day_df = train_df[d*num_feature:(d+1)*num_feature]
+        day_df = train_df[d*num_feature_t:(d+1)*num_feature_t]
         day_df = day_df.drop(day_df.columns[0:3], axis=1)
         
+        # For x in 0~14th hour, y = [9~23rd PM2.5's]
+        pm25 = list(day_df.iloc[pm25_index,num_prev_hour:24])
+        
+        # Drop specified features
+        day_df = day_df.drop(day_df.index[[list(feature_dropped_idx)]])
+        
         for hr in range(24-num_prev_hour):
-        # keep last-hour PM2.5 value
         
             df_prev = day_df.iloc[:,hr:(hr+num_prev_hour)]
             df_prev = df_prev.as_matrix()
             x = list(df_prev.flatten())
             
-            y_hat = day_df.iloc[:,hr+num_prev_hour].iloc[pm25_index]
+            y_hat = pm25[hr]
         
             # output processed data in form:
             # each line starts with y_hat, followed by x's seperated by comma
