89ed83e0a984175e8cdcaeef7198410ac11923f6
mhff0 <r06922030@ntu.edu.tw>
2017-10-09 17:46:34 +0800

    clean old codes

--- hw1/hw1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chmod +x linear_test.py
-./linear_test.py model.out $1 > $2
--- hw1/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./preprocess.py data/train.csv train.in validation.in
-./linear_train.py train.in validation.in model.out
