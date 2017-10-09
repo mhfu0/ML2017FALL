093668773956e2d126dee96a11389e0f067af3d4
mhff0 <r06922030@ntu.edu.tw>
2017-10-09 17:47:09 +0800

    correction on testing

--- hw1/train_test_v2.py
+++ hw1/train_test_v2.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # Linear regresssion
-# ./linear_train.py [train] [test] [model]
+# ./train_test_v2.py [train] [test] [model]
 
 import sys
 import numpy as np
 
 feature_set=[7,9]
-prev_hrs=9
+prev_hrs=5
+iteration=25000
+drop=120
+ld = 0.01
 
 def main(argv):
     np.random.seed(1)
@@ -50,32 +53,40 @@ def main(argv):
                 if f in feature_set:
                     x[(480-prev_hrs)*m+hr] += train_data[m][f][hr:hr+prev_hrs]
    
+    # Concatenate with some quadratic terms
+    for n in range(len(x)):
+        row = x[n]
+        x[n].append(row[-1] ** 2)
+        x[n].append(row[-2] ** 2)
+    
+    # Seperate some data for validation
+    x_val = []
+    y_val = []
+    
+    x_tmp = []
+    y_tmp = []
+    for n in range(len(x)):
+        if n % (480-prev_hrs) < ((480-prev_hrs)-drop):
+            x_tmp.append(x[n])
+            y_tmp.append(y_hat[n])
+        else:
+            x_val.append(x[n])
+            y_val.append(y_hat[n])
+    
+    x = np.array(x_tmp)
+    y_hat = np.array(y_tmp)
+    
+    x_val = np.array(x_val)
+    y_val = np.array(y_val)
+    
     # Regression with gradient descent
     # dim(x)=len(x) by (prev_hrs*# features)
     # dim(y_hat)=len(x)
-
-    # Randomly drop some data
-    drop = len(x)//4
-    for i in range(drop):
-        d = np.random.randint(len(x))
-        del x[d]
-        del y_hat[d]
-    sys.stderr.write('%d\n' % len(x))
-    
-    # Let x <- x concat. with some square terms
-    for i in range(len(x)):
-        row = x[i]
-        x[i].append(row[-1] ** 2)
-        x[i].append(row[-2] ** 2)
-    
-    x = np.array(x)
-    y_hat = np.array(y_hat)
     
     # y = b + sum(w_Tx)
     # Randomly determine model parameters
     b = 1
     w = np.random.randn(len(x[0]))
-    ld = 0.001
 
     lr = 1
     ada_b = 0.0
@@ -84,7 +95,6 @@ def main(argv):
     prev_loss = float('inf')
 
     # Gradient descent with adagrad
-    iteration = 25000
     for k in range(iteration):
         b_grad = 0.0
         w_grad = np.zeros(len(w))
@@ -121,6 +131,17 @@ def main(argv):
 
     sys.stderr.write('Training ended with total loss: %f\n' % loss)
     
+    # Validation
+    sys.stderr.write('=====validation=====\n')
+    
+    y = np.dot(x_val,w) + b
+    dy = y_val - y
+    loss = sum(np.power(dy,2)) / len(x_val)
+    rmse = np.sqrt(sum(np.power(dy,2)) / len(x_val))
+    sys.stderr.write('Total loss on validation set: %f\n' % loss)
+    sys.stderr.write('RMSE on validation set: %f\n' % rmse)
+    
+    
     # Save model
     with open(argv[3], 'w') as output:
         line = ''
@@ -143,7 +164,7 @@ def main(argv):
         x = []
         for f in range(18):
             if f in feature_set:
-                x += test_raw[i*18+f][2:2+prev_hrs]
+                x += test_raw[i*18+f][2+(9-prev_hrs):11]
         
         x = list(map(float, x))
         row = x
