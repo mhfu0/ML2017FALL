f50bcbc165cc702e4d25576beee7fd04a06a455c
mhfu0 <frank0319@gmail.com>
2017-10-21 11:58:22 +0800

    nn_classifier

--- hw2/train.py
+++ hw2/train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import tensorflow as tf
 import numpy as np
 from keras.backend.tensorflow_backend import set_session
@@ -38,7 +39,8 @@ with open('data/X_train', 'r') as f:
         X.append(line)
 
 X=np.array(X)
-X_n=X/X.max(axis=0)
+XMAX=X.max(axis=0)
+X_n=X/XMAX
 X=X_n
 
 Y=[]
@@ -59,23 +61,46 @@ y_train=Y[:25000]
 y_test=Y[25000:]
 
 y_train=np_utils.to_categorical(y_train)
-y_test=np_utils.to_categrical(y_test)
+y_test=np_utils.to_categorical(y_test)
 
 model = Sequential()
 model.add(Dense(64,input_shape=(106,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(128))
-model.add(Activation('relu'))
+model.add(Activation('tanh'))
 model.add(Dropout(0.2))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(128))
+model.add(Activation('tanh'))
+model.add(Dropout(0.2))
+
+
+model.add(Dense(2))
+model.add(Activation('softmax'))
 rms=RMSprop()
 model.compile(loss='categorical_crossentropy', metrics=['accuracy'],optimizer=rms)
 
 model.fit(x_train,y_train,
-          batch_size=32,epochs=1000,
+          batch_size=32,epochs=10,
           verbose=1)
-score=model.evaluate(x_test,y_test,show_accuracy=True,verbose=1)
+model.summary()
+
+score=model.evaluate(x_test,y_test,verbose=1)
 print(score)
+
+testX=[]
+with open('data/X_test','r') as f:
+    f.readline()
+    for line in f:
+        line = line.strip('\n').split(',')
+        line = list(map(int,line))
+        testX.append(line)
+
+testX=np.array(testX)
+testX=testX/XMAX
+
+testY=model.predict_classes(testX, verbose=1)
+
+with open(sys.argv[1], 'w') as f:
+    f.write('id,label\n')
+    for i, l in enumerate(testY):
+        f.write('%d,%d\n' % ((i+1),l))
+
