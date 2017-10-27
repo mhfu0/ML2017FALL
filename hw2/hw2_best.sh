e308937b59dc7fb9047df87de2ef0dd29ba13ed1
mhfu0 <frank0319@gmail.com>
2017-10-27 18:34:27 +0800

    update best model

--- hw2/hw2_best.sh
+++ hw2/hw2_best.sh
@@ -3,5 +3,9 @@
 #$3: provided train feature (X_train)  $4: provided train label (Y_train)
 #$5: provided test feature (X_test)    $6: prediction.csv
 
+# NN model
 #python3 ./nn_train_test.py 1 model/model_best.h5 $3 $4 $5 $6
-python3 ./nn_train_test.py 0 model/model_best.h5 $3 $4 $5 $6
+#python3 ./nn_train_test.py 0 model/model_best.h5 $3 $4 $5 $6
+
+# Xgboost with default settings
+python3 ./xgb_train_test.py $3 $4 $5 > $6
---
+++ hw2/xgb_train_test.py
@@ -0,0 +1,60 @@
+import sys
+import numpy as np
+import pandas as pd
+
+from xgboost import XGBClassifier
+from tabulate import tabulate
+
+def normalize(X, testX):
+    allX = np.concatenate((X, testX)).astype(float)
+    mu = sum(allX) / len(allX)
+    sigma = np.std(allX, axis=0)
+    
+    for i in range(len(allX)):
+        allX[i]=(allX[i]-mu)/sigma
+    return allX[0:len(X)], allX[len(X):]
+
+if __name__=='__main__':
+    np.random.seed(7)
+
+    col_name=[]
+    X=[]
+    with open(sys.argv[1], 'r') as f:
+        col_name = f.readline()
+        col_name = col_name.strip('\n').split(',')
+        for line in f:
+            line = line.strip('\n').split(',')
+            line = list(map(int,line))
+            X.append(line)
+    X=np.array(X)
+    XMAX=X.max(axis=0)
+
+    Y=[]
+    with open(sys.argv[2], 'r') as f:
+        f.readline()
+        for line in f:
+            line = line.strip('\n').split(',')
+            line = list(map(int,line))
+            Y.append(line)
+    Y=np.array(Y).astype('int32')
+    Y=np.ravel(Y)
+
+    testX=[]
+    with open(sys.argv[3],'r') as f:
+        f.readline()
+        for line in f:
+            line = line.strip('\n').split(',')
+            line = list(map(int,line))
+            testX.append(line)
+    testX=np.array(testX)
+    
+    # Normalization
+    X, testX = normalize(X, testX)
+    
+    model = XGBClassifier()
+    model.fit(X, Y)
+    y_ = model.predict(testX)
+    
+    print('id,label')
+    for i,v in enumerate(y_):
+        print('%d,%d'%(i+1,v))
