cef0ec4b8aff6accce51a6188eb3bf09475deb71
mhfu0 <frank0319@gmail.com>
2017-12-08 02:52:03 +0800

    add model DL

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget -c -O ./model_semi_3.h5 'https://www.dropbox.com/s/v198ulklbgjfyzy/model_semi_3.h5?dl=1'
+wget -c -O ./model_semi_0.h5 'https://www.dropbox.com/s/hvgx424mwbh1sql/model_semi_0.h5?dl=1'
+wget -c -O ./model_semi_1.h5 'https://www.dropbox.com/s/oarf3z0p2dkxjkl/model_semi_1.h5?dl=1'
+wget -c -O ./model_semi_2.h5 'https://www.dropbox.com/s/a1jmjzed4qvztuj/model_semi_2.h5?dl=1'
 python3 ./rnn_test.py $1 $2
