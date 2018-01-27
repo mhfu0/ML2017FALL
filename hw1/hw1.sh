09c29f7d5fd2c0e07cd809c6e148d5ef600ea1e0
mhfu0 <r06922030@ntu.edu.tw>
2018-01-27 19:24:20 +0800

    small fix

--- hw1/hw1.sh
+++ hw1/hw1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#python3 src/train_linear.py data/train.csv model/model_linear
-python3 src/test_linear.py model/model_linear $1 > $2
+#python3 train_linear.py data/train.csv model/model_linear
+python3 test_linear.py model/model_linear $1 > $2
--- hw1/hw1_best.sh
+++ hw1/hw1_best.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#python3 src/train_best.py data/train.csv model/model_best
-python3 src/test_best.py model/model_best $1 > $2
+#python3 train_best.py data/train.csv model/model_best
+python3 test_best.py model/model_best $1 > $2
